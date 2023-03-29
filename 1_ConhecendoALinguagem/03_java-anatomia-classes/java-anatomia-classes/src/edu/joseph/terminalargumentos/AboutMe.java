236d4e3684744eaf630676a834ff60286ffd1aba
JosephAlves0 <j.eduardo.alves@hotmail.com>
2023-03-29 20:48:17 -0300

    adicionando logica condicional e controle de fluxos

--- 1_ConhecendoALinguagem/03_java-anatomia-classes/java-anatomia-classes/src/edu/joseph/terminalargumentos/AboutMe.java
+++ 1_ConhecendoALinguagem/03_java-anatomia-classes/java-anatomia-classes/src/edu/joseph/terminalargumentos/AboutMe.java
@@ -22,5 +22,11 @@ public class AboutMe {
         System.out.println("Olá! Me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + " cm");
+
+            int numero1 = 1;
+            String numero2 = "2";
+            System.out.println(numero1+numero2);
+
+
     }
 }
