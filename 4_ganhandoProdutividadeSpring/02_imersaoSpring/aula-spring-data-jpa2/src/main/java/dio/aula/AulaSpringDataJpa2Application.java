663506ec84bb5ed3219ec73a430d5ede819352ec
JosephAlves0 <j.eduardo.alves@hotmail.com>
2023-04-28 20:32:04 -0300

    Atualização dos cursos executados

---
+++ 4_ganhandoProdutividadeSpring/02_imersaoSpring/aula-spring-data-jpa2/src/main/java/dio/aula/AulaSpringDataJpa2Application.java
@@ -0,0 +1,15 @@
+package dio.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSpringDataJpa2Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(AulaSpringDataJpa2Application.class, args);
+
+	}
+
+}
